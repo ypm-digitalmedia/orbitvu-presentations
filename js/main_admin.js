015cea30ea792bf242fe0d198db03da228386083
Andy Melien <ypm.digitalmedia@gmail.com>
2020-08-18 17:44:26 -0400

    probably done

--- js/main_admin.js
+++ js/main_admin.js
@@ -27,9 +27,9 @@ $(document).ready(function () {
 	});
 	
 template = "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3' id='item%%num%%'>";
-		template += "<a href='%%path%%' class='presentation-link'>";
+		template += "<a href='%%path%%' class='presentation-link' %%hoverString%%>";
 		template += "<div class='presentation-container'>";
-		template += "<div class='presentation-thumb' style='background-image:%%thumb%%'></div>";
+		template += "<div id='t_%%idnum%%' class='presentation-thumb %%hoverClass%%' style='background-image:%%thumb%%'></div>";
     template += "<div class='presentation-text'>";
     template += "<h4>%%title%%</h4>";
     template += "<h5 class='item-composition'>%%composition%%</h5>";
@@ -41,7 +41,32 @@ template = "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3' id='item%%num%%'>"
 		template += "</a>";
 		template += "</div>";
 
+
+
 });
+
+function hoverImgOn(theId) {
+	// console.log("ON: " + theId );
+	$("#t_"+theId).css("background-image","url('img/_thumb/Spann_TN_"+theId+"_t.jpg')");
+}
+
+function hoverImgOff(theId) {
+	// console.log('OFF: ' + theId);
+	$("#t_"+theId).css("background-image","url('img/_thumb-silo/Spann_TN_"+theId+"_ts.jpg')");
+}
+
+function getId(str) {
+	if ( str.indexOf('Thumbnail #') > -1 ) {
+		var num = str.split("#")[1];
+		return pad(num,3);
+	}
+}
+
+function pad(n, width, z) {
+	z = z || '0';
+	n = n + '';
+	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
 
 function makeGrid() {
   	
@@ -62,6 +87,8 @@ function makeGrid() {
 		// if( admin || shown.indexOf(index) > -1 ) {
 		if( shown == [0] || shown.indexOf(index) > -1 ) {
 			var thumbString = 'url("'+thumbpath+item[7]+'_t.jpg")';
+			var thumbHoverClass = "";
+			var thumbHoverString = "";
 			var titleString = item[3];
 			var idString = item[2];
 			var sidString = item[1] + " (" + item[0] + ")";
@@ -69,9 +96,11 @@ function makeGrid() {
 			var pathString = presentationpath + item[7] + ".html";
 			var pathString = "view_admin.html?sid=" + item[1].split("Spann ").join("");
 			var locString = item[6];
-			var extraString = "";
+			var idNum = getId(item[0]);
 		} else {
 			var thumbString = 'url("'+thumbpathsilo+item[7]+'_ts.jpg")';
+			var thumbHoverClass = "unavailable";
+			var thumbHoverString = "onmouseover='hoverImgOn(\"%%id%%\")' onmouseout='hoverImgOff(\"%%id%%\")'";
 			var titleString = item[3];
 			var idString = item[2];
 			var sidString = item[1] + " (" + item[0] + ")";
@@ -79,6 +108,7 @@ function makeGrid() {
 			var pathString = presentationpath + item[7] + ".html";
 			var pathString = "view_admin.html?sid=" + item[1].split("Spann ").join("");
 			var locString = item[6];
+			var idNum = getId(item[0]);
 			sidString += "<br /><span style='font-weight: bold; color: darkred'>Images unavailable</span>";
 		}
 
@@ -92,6 +122,10 @@ function makeGrid() {
 			html = html.split("%%title%%").join(titleString);
 			html = html.split("%%composition%%").join(compString);
 			html = html.split("%%location%%").join(locString);
+			html = html.split("%%hoverClass%%").join(thumbHoverClass);
+			html = html.split("%%idnum%%").join(idNum);
+			html = html.split("%%hoverString%%").join(thumbHoverString.split("%%id%%").join(getId(item[0])));
+			
 			
 			$("#items_row").append(html);
     }
